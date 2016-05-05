0f8e57781ec9c5c2dd1a43fd002371dda0c03016
Wuwei Lin <vincentl13x@gmail.com>
2016-05-06 00:08:20 +0800

    Add improved APIUtil

--- server/src/APIUtil.cpp
+++ server/src/APIUtil.cpp
@@ -1,20 +1,25 @@
 #include "APIUtil.h"
+#include "TradeRecord.h"
 #include <sstream>
 
-void APIUtil::securityLock(APIUtil::StmtPtr stmt, const std::string &symbol) {
-    std::istringstream s;
-    s << "CALL security_lock('" + symbol + "')";
+void APIUtil::securityTryLock(APIUtil::StmtPtr stmt, const std::string &symbol, bool &result) {
+    std::ostringstream s;
+    s << "CALL security_try_lock('" << symbol << "', @result)";
     stmt->execute(s.str());
+    std::shared_ptr<sql::ResultSet> results(stmt->executeQuery("SELECT @result AS _result"));
+    results->next();
+    result = results->getBoolean("_result");
+    results->next();
 }
 
 void APIUtil::securityUnlock(APIUtil::StmtPtr stmt, const std::string &symbol) {
-    std::istringstream s;
-    s << "CALL security_unlock('" + symbol + "')";
-    stmt->execute(query);
+    std::ostringstream s;
+    s << "CALL security_unlock('" << symbol << "')";
+    stmt->execute(s.str());
 }
 
 void APIUtil::orderbookPut(APIUtil::StmtPtr stmt, const Quote &quote) {
-    std::istringstream s;
+    std::ostringstream s;
     s << "CALL orderbook_put("
     << "'" << quote.symbol.c_str() << "',"
     << "'" << quote.client.c_str() << "',"
@@ -26,19 +31,38 @@ void APIUtil::orderbookPut(APIUtil::StmtPtr stmt, const Quote &quote) {
 }
 
 void APIUtil::orderbookDelete(APIUtil::StmtPtr stmt, const int quote_id) {
-    std::istringstream s;
+    std::ostringstream s;
     s << "CALL orderbook_delete(" << quote_id << ")";
     stmt->execute(s.str());
 }
 
 void APIUtil::orderbookQueryMatch(StmtPtr stmt, const std::string &symbol) {
-    std::istringstream s;
-    s << "CALL orderbook_query_match('" << symbol << ")";
+    std::ostringstream s;
+    s << "CALL orderbook_query_match('" << symbol << "')";
     stmt->execute(s.str());
 }
 
 void APIUtil::orderbookUpdate(StmtPtr stmt, const int quote_id, const int quantity) {
-    std::istringstream s;
+    std::ostringstream s;
     s << "CALL orderbook_update(" << quote_id << "," << quantity << ")";
     stmt->execute(s.str());
 }
+
+void APIUtil::systemStatusIsRunning(StmtPtr stmt, bool &result) {
+    stmt->execute("CALL systemstatus_is_running(@result)");
+    std::shared_ptr<sql::ResultSet> results(stmt->executeQuery("SELECT @result AS _result"));
+    results->next(); //only one result exists
+    result = results->getBoolean("_result");
+    results->next(); //free old results
+}
+
+void APIUtil::tradeRecordPut(StmtPtr stmt, const TradeRecord &record) {
+    std::ostringstream s;
+    s << "CALL traderecord_put("
+    << record.order_buy << ","
+    << record.order_sell << ","
+    << record.price << ","
+    << record.quantity << ")";
+    stmt->execute(s.str());
+}
+
--- server/src/APIUtil.h
+++ server/src/APIUtil.h
@@ -2,19 +2,23 @@
 #define EXCHANGESIMULATOR_SERVER_APIUTIL_H
 
 
+#include "Quote.h"
+#include "TradeRecord.h"
 #include <string>
 #include <memory>
-#include <mysql_connection.h>
+#include <cppconn/statement.h>
 
 class APIUtil {
 public:
     typedef std::shared_ptr<sql::Statement> StmtPtr;
-    static void securityLock(StmtPtr stmt, const std::string &symbol);
+    static void securityTryLock(StmtPtr stmt, const std::string &symbol, bool &result);
     static void securityUnlock(StmtPtr stmt, const std::string &symbol);
     static void orderbookPut(StmtPtr stmt, const Quote &quote);
     static void orderbookDelete(StmtPtr stmt, const int quote_id);
     static void orderbookQueryMatch(StmtPtr stmt, const std::string &symbol);
     static void orderbookUpdate(StmtPtr stmt, const int quote_id, const int quantity);
+    static void systemStatusIsRunning(StmtPtr stmt, bool &result);
+    static void tradeRecordPut(StmtPtr stmt, const TradeRecord &record);
 };
 
 
